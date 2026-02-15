d7767ac6dcd043a7222cf936e554775e2f87b6ea
Garpheild <152085836+Garpheild@users.noreply.github.com>
2026-02-15 23:12:46 +0500

    Вторая версия

--- src/lab-9-v15/MarkArray.cs
+++ src/lab-9-v15/MarkArray.cs
@@ -11,12 +11,31 @@ public class MarkArray
         CollectionCount++;
     }
 
+    public MarkArray(int size, int minMark, int maxMark)
+    {
+        if (size <= 0)
+            throw new ArgumentException("Размер должен быть положительным числом");
+        marks = new Mark[size];
+        Random random = new Random();
+        string[] subjects = {
+                "Алгебра", "Физика", "Химия", "Биология",
+                "История", "Литература", "Информатика", "Английский",
+                "География", "Физкультура"
+            };
 
+        for (int i = 0; i < size; i++)
+        {
+            string subject = subjects[random.Next(subjects.Length)];
+            int mark = random.Next(minMark, maxMark+1);
+            marks[i] = new Mark(subject, mark);
+        }
+        CollectionCount++;
+    }
 
     public MarkArray(int size)
     {
         if (size <= 0)
-            throw new ArgumentException("Размер должен быть положительным числом", nameof(size));
+            throw new ArgumentException("Размер должен быть положительным числом");
 
         Random random = new Random();
         marks = new Mark[size];
@@ -31,15 +50,6 @@ public class MarkArray
 
     public MarkArray(MarkArray other)
     {
-        if (other == null)
-            throw new ArgumentNullException(nameof(other));
-
-        //if (other.marks == null)
-        //{
-        //    marks = null;
-        //    return;
-        //}
-
         marks = new Mark[other.marks.Length];
         for (int i = 0; i < other.marks.Length; i++)
         {
@@ -50,36 +60,28 @@ public class MarkArray
         }
         CollectionCount++;
     }
-
-
-    public void Display()
+    public string Display()
     {
+        string s = "";
         if (marks == null || marks.Length == 0)
         {
-            Console.WriteLine("Коллекция пуста");
-            return;
+            s += ("Коллекция пуста");
+            return s;
         }
 
-        Console.WriteLine("Элементы коллекции:");
         for (int i = 0; i < marks.Length; i++)
         {
             if (marks[i] != null)
             {
-                Console.WriteLine($"marks[{i}] = {marks[i].Show()}");
-            }
-            else
-            {
-                Console.WriteLine($"marks[{i}] = null");
+                s += ($"marks[{i}] = {marks[i].Show()}\n");
             }
         }
-        Console.WriteLine();
+        return s;
     }
-
-
 
     public int Length
     {
-        get { return marks?.Length ?? 0; }
+        get { return marks.Length; }
     }
 
     public Mark this[int index]
@@ -97,4 +99,5 @@ public class MarkArray
             marks[index] = value;
         }
     }
+
 }
